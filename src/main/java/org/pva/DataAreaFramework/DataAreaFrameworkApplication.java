02575ecc2a72a4faf2d51733db17cc713517ff68
ViktorVx <victorptrv@yandex.ru>
2020-07-09 20:40:45 +0300

    Init commit

---
+++ src/main/java/org/pva/DataAreaFramework/DataAreaFrameworkApplication.java
@@ -0,0 +1,13 @@
+package org.pva.DataAreaFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAreaFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAreaFrameworkApplication.class, args);
+	}
+
+}
